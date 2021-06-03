d61f25adfe438ffee3e82bf89f470a74e74bd3ae
Noemi Turner <nturner3@zagmail.gonzaga.edu>
2021-06-03 12:59:55 -0700

    Added display main menu function

--- fashion_project.cpp
+++ fashion_project.cpp
@@ -4,3 +4,31 @@
 // Date: Summer 2021
 // Desc: fashion business software project
 //----------------------------------------------------------------------
+
+#include "fashion_project.h"
+
+
+//----------------------------------------------------------------------
+// Default Destructor
+//----------------------------------------------------------------------
+FashionSoftware::~FashionSoftware() 
+{
+
+}
+
+
+//----------------------------------------------------------------------
+// Displays main menu for the fashion software program
+//
+// Conditions:
+//   none
+// Inputs:
+//   none
+// Outputs:
+//   none
+//----------------------------------------------------------------------
+void FashionSoftware::display_main_menu(string company_name)
+{
+  cout << "-----------" << company_name << "Software Main Menu-----------" <<endl;
+  cout << "1. POS System" <<endl;
+}
--- fashion_project.h
+++ fashion_project.h
@@ -4,3 +4,23 @@
 // Date: Summer 2021
 // Desc: fashion business software project
 //----------------------------------------------------------------------
+
+#ifndef FASHION_PROJECT_H
+#define FASHION_PROJECT_H
+
+class FashionSoftware
+{
+public:
+    // default constructor is implied
+
+    // default destructor
+    virtual ~FashionSoftware() {};
+    
+    virtual void display_main_menu(string company_name);
+
+private:
+    // private member variables and functions go here
+
+};
+
+#endif
